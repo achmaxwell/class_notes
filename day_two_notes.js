f985e26747242df664d4b9b65b35caf331488879
Amelia Heaton <ameliaheaton@Amelias-MacBook-Pro.local>
2021-03-25 20:56:11 -0400

    Added more git info

--- day_two_notes.js
+++ day_two_notes.js
@@ -18,3 +18,18 @@
 // cd .. > moves you up
 // ls > lists items inside existing folder
 // pwd > specifies directory path you're currently in
+
+// To initiate a git repository
+// git init
+// Staging files for a commit
+//git status
+// git add <file_name> <-- file name that I would want to add OR git add . 
+// to add all of the files inside of the folder that you are in and stage them for commit
+// git reset will revert back
+
+// To commit your changes, run the following command:
+// git commit -m "Your commit message here"
+
+// To view the commit:
+// git log
+// Shows us commit hash, branch name, commit message, and other metadata
