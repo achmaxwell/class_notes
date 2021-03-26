1f15fcb03fcc53b4423db679a12facadbfa7787b
Amelia Heaton <ameliaheaton@Amelias-MacBook-Pro.local>
2021-03-25 20:39:37 -0400

    intialized class notes

---
+++ day_two_notes.js
@@ -0,0 +1,20 @@
+// var vs let vs const
+
+//Let and var can be reassigned when run
+//let name; 'Amelia'
+//name = 'Amelia'
+//console.log(name);
+
+//Const Cannot be reassigned or will not change
+//const number = 15
+//console.log(number)
+
+//let x = 10;
+
+// Git
+// Version control system designed to keep chronological timeline of all of your code changes.
+// To traverse file system in terminal:
+// cd name_of_folder > moves you down
+// cd .. > moves you up
+// ls > lists items inside existing folder
+// pwd > specifies directory path you're currently in
