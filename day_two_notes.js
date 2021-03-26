6ac03ded604aeaaa851c5742d8c7f1712a56dd71
Amelia Heaton <ameliaheaton@Amelias-MacBook-Pro.local>
2021-03-25 21:17:26 -0400

    added notes

--- day_two_notes.js
+++ day_two_notes.js
@@ -22,7 +22,7 @@
 // To initiate a git repository
 // git init
 // Staging files for a commit
-//git status
+// git status
 // git add <file_name> <-- file name that I would want to add OR git add . 
 // to add all of the files inside of the folder that you are in and stage them for commit
 // git reset will revert back
@@ -33,3 +33,13 @@
 // To view the commit:
 // git log
 // Shows us commit hash, branch name, commit message, and other metadata
+
+// Go to GitHub.com and create the repository
+// Name or repository, make sure to leave it public and keep all other options deselected
+// Click create repository
+// Copy the HTTPS url provided (one that ends with .git)
+// Go back to VScode
+// Link you repository with remote repository by typing:
+// git remote add origin <paste the link>
+// To push to remote repository
+// git push origin master
